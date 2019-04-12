6578c3a9b6342eed7a36a88fe206f9e4461fe074
cmengs <48946399+cmengs@users.noreply.github.com>
2019-04-12 10:21:54 +0800

    提交修改

--- src/main/java/cn/m/c/TestC.java
+++ src/main/java/cn/m/c/TestC.java
@@ -2,6 +2,7 @@ package cn.m.c;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,4 +13,12 @@ public class TestC {
 	public String hello(){
 		return "hello";
 	}
+	
+	//TODO 这是一个bug
+	@RequestMapping("/hello1")
+	public String hello1(@RequestParam(name="message",required=false) String message){
+		return message + "您输入的字符个数："+message.length();
+	}
+	
+	
 }
