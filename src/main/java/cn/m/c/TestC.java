99aef67ff198fdbdca518ee7af3d83b0bb1e7ca6
cmengs <48946399+cmengs@users.noreply.github.com>
2019-04-12 09:45:09 +0800

    第一次上传

---
+++ src/main/java/cn/m/c/TestC.java
@@ -0,0 +1,15 @@
+package cn.m.c;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestC {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello";
+	}
+}
